7f7928b3dfcd75c7efe45db8ba5810954de25181
onnurd <134932514+onnurd@users.noreply.github.com>
2024-07-31 11:53:21 +0300

    Sidebar added.
    Sidebar and chat boxes design have been added to it.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Chat from './components/Chat';
 import TopBar from './components/TopBar';
-
+import Sidebar from './components/Sidebar';
 const App: React.FC = () => {
   return (
     <div className="App">
       <TopBar />
-      <Chat />
+      <div className="main-content">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Chat 1</li>
+        <li>Chat 2</li>
+        <li>Chat 3</li>
+        <li>Chat 4</li>
+        <li>Chat 5</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
